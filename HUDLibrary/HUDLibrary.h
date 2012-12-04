7f19dd9cfae0b86b9f5790a91ae67f4c31f50b54
mao yu <maoyu417@163.com>
2012-12-04 18:02:06 +0800

    库新项目文件

---
+++ HUDLibrary/HUDLibrary.h
@@ -0,0 +1,13 @@
+//
+//  HUDLibrary.h
+//  HUDLibrary
+//
+//  Created by lixiaoyu on 12-12-4.
+//  Copyright (c) 2012年 Liu&Mao. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface HUDLibrary : NSObject
+
+@end
---
+++ HUDLibrary/HUDLibrary.m
@@ -0,0 +1,13 @@
+//
+//  HUDLibrary.m
+//  HUDLibrary
+//
+//  Created by lixiaoyu on 12-12-4.
+//  Copyright (c) 2012年 Liu&Mao. All rights reserved.
+//
+
+#import "HUDLibrary.h"
+
+@implementation HUDLibrary
+
+@end
